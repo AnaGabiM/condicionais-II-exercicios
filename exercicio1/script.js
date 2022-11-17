1325282262d4a7f62a6c99d0aae670f6df8eba5a
AnaGabiM <ana.agma98@gmail.com>
2022-11-16 22:10:05 -0300

    Exercicio Condicionais2 - Ozemela

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,24 @@
 console.log("teste 1")
+const numero = prompt ("Digite um número");
 
+//1
+if (numero % 2 !== 0 ) {
+    if (numero % 3 === 0) {
+    console.log("Divisivel por 3")
+    } else {
+        console.log("Nao divisivel por 2 ou 3")
+    }
+} else {
+    console.log("Divisivel por 2")
+}
+
+//2
+if (numero % 2 === 0 || numero % 3 === 0) {
+    if (numero % 2 === 0) {
+        console.log("Divisivel por 2")
+    } else {
+        console.log("Divisivel por 3")
+    }
+} else{
+    console.log("Não divisivel por 2 ou 3")
+}
